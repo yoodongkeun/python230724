d2333b60b0063ed10d9d473a65b96e0163c25f5f
dbehdrms@1 <ydk02124078@gmail.com>
2023-07-25 16:22:02 +0900

    오늘 7.25

--- BankAccount.py
+++ BankAccount.py
@@ -12,8 +12,8 @@ class BankAccount:
     def withdraw(self, amount):
         self.__balance -= amount
     def __str__(self):
-        return "{0} , {1} , {2}".format(self.id, \
-            self.name, self.balance)
+        return "{0} , {1} , {2}".format(self.__id, \
+            self.__name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
--- 상속01.py
+++ 상속01.py
@@ -6,17 +6,29 @@ class Person:
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
+    def working(self):
+        print("현재 작업중")
+    def coding(self):
+        print('현재 코딩중')
+
 class Student(Person):
+    # 상속받고 덮어쓰기(재정의, override)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #명시적으로 부모클래스 초기화 메서드 호출
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
-
-
+    
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(self.Subject, self.StudentID))
 p = Person("전우치", "010-222-1234")
-s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
+s = Student("이순신", "010-111-1234", "빅데이터", "201234")
+#print(p.__dict__)
+#print(s.__dict__)
 
+p.printInfo()
+s.printInfo()
+s.coding()
+s.working()
 
