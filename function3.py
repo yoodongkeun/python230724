48bf1f2c1bb1a87cb0516c22414e42c5231da226
dbehdrms@1 <ydk02124078@gmail.com>
2023-07-25 10:51:23 +0900

    수열연습

--- function3.py
+++ function3.py
@@ -11,3 +11,23 @@ def union(*ar):
 print(union("HAM", "SPAM"))
 print(union("HAM", "SPAM", "EGG"))
 
+g= lambda x,y:x*y
+print(g(3,4))
+print(g(5,6))
+print((lambda x:x*x)(3))
+print(globals())
+
+# 내장함수 필터
+def getBiggerThan20(i):
+    return i >20
+
+lst = [10,25,30]
+iterL = filter(getBiggerThan20, lst)
+for i in iterL:
+    print(i)
+
+print (" -----람다함수----")
+iterL = filter(lambda x:x>20, lst)
+for i in iterL:
+    print(i)
+
