d809323f6e677e6b4242312247ac153c29024e60
dbehdrms@1 <ydk02124078@gmail.com>
2023-07-26 09:52:59 +0900

    파일 연습2

--- DemoFile.py
+++ DemoFile.py
@@ -22,8 +22,19 @@ print("{0:.2f}".format(4/3))
 f= open("c:\\work\\demo.txt", "wt", encoding="utf-8")
 f.write("첫번재\n두번째\n세번째\n")
 f.close()
-
+#파일읽기
 f= open(r"c:\work\demo.txt", "rt", encoding="utf-8")
 result = f.read()
+print("----라인단위-----")
+f.seek(0)
+print(f.readline(), end="")
+print(f.readline(), end="")
+print("----리스트로받기----")
+f.seek(0)
+result = f.readlines()
+print(result)
+
 f.close()
 print(result)
+
+#
