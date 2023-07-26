db3e428b429e74875033f54c92f9c59e8757adda
dbehdrms@1 <ydk02124078@gmail.com>
2023-07-26 16:09:15 +0900

    데이터베이스 연습2

--- db2.py
+++ db2.py
@@ -1,8 +1,8 @@
-# db1.py
+# db2.py
 import sqlite3
 
-#연결객체 생성
-con =sqlite3.connect(":memory:")
+#연결객체 생성(파일에 저장)
+con =sqlite3.connect("c:\\work\\sample.db")
 # 커서 객체
 cur = con.cursor()
 # 테이블 구조 생성
@@ -24,15 +24,8 @@ cur.executemany("insert into PhoneBook (name, phoneNum) values" +
 
 # 검색구문
 cur.execute("select * from PhoneBook;")
-print("----fetchone()------")
-print(cur.fetchone())
-print("-----fetchmany(2)----")
-print(cur.fetchmany(2))
-print("----fetchall()-----")
 print(cur.fetchall())
 
-
-
-# for row in cur:
-#     print(row)
+#작업을 완료 ---> commit을 안하면 파일에 저장안됨.
+con.commit()
 
